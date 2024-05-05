fc63e55ab1ce4e5c0d59ba1bb2cf8e45e17eccdd
manu <emanuele.pocelli@mail.polimi.it>
2024-05-05 21:52:03 +0200

    tentative solution, need to tweak the agent

--- Deliverables/Requirement1/pricing_environment.py
+++ Deliverables/Requirement1/pricing_environment.py
@@ -1,9 +1,11 @@
+import numpy as np
 class PricingEnvironment:
-    def __init__(self, distribution, cost):
-        self.distribution=distribution
+    def __init__(self, norm_distribution, cost,num_cust):
+        self.norm_distribution=norm_distribution
         self.cost = cost
+        self.num_cust=num_cust
 
     def round(self, p_t, n_t):
-        d_t = self.distribution(p_t,n_t)
+        d_t = np.random.binomial(n_t,self.norm_distribution(p_t))
         r_t = (p_t - self.cost)*d_t
         return d_t, r_t

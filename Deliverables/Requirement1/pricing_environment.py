b9837a9782a14aeb71917c06ffe4f0e9008706c3
manu <emanuele.pocelli@mail.polimi.it>
2024-05-07 11:41:33 +0200

    high level update

--- Deliverables/Requirement1/pricing_environment.py
+++ Deliverables/Requirement1/pricing_environment.py
@@ -3,9 +3,8 @@ class PricingEnvironment:
     def __init__(self, norm_distribution, cost,num_cust):
         self.norm_distribution=norm_distribution
         self.cost = cost
-        self.num_cust=num_cust
 
-    def round(self, p_t, n_t):
-        d_t = np.random.binomial(n_t,self.norm_distribution(p_t))
+    def round(self, p_t,num_cust):
+        d_t = np.random.binomial(num_cust,self.norm_distribution(p_t))
         r_t = (p_t - self.cost)*d_t
         return d_t, r_t
---
+++ Deliverables/utils.py
@@ -0,0 +1,16 @@
+class PricingEnvironment:
+    def __init__(self):
+        pass
+
+    def round(self):
+        pass
+
+class Agent:
+    def __init__(self, valuation, budget, T, eta):
+        pass
+
+    def pull_arm(self):
+        pass
+    
+    def update(self):
+        pass

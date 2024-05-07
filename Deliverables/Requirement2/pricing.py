45f8551a503b5259bdfe66c49671e3ac916493bb
Daniele Giangregorio <daniele.giangregorio00@gmail.com>
2024-05-07 11:23:15 +0200

    Requirement 1 high level update

--- Deliverables/Requirement2/pricing.py
+++ Deliverables/Requirement2/pricing.py
@@ -1,52 +1,25 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-class PricingStrategy:
-    def __init__(self):
-        self.prices = np.linspace(0, 1, 100)  # 100 price levels
-
-    def discretize_prices(self):
-        # Discretize the continuous set of prices
-        return self.prices
-
-class Auction:
-    def __init__(self):
-        self.bids = None
-
-    def run(self, bids):
-        # Run a generalized first-price auction
-        self.bids = bids
-        return np.argmax(self.bids)  # The highest bid wins
-
-class LearningAlgorithm:
-    def __init__(self):
-        self.auction_result = None
-
-    def update(self, auction_result):
-        # Update the learning algorithm based on the auction result
-        self.auction_result = auction_result
-
-class PrimalDualAlgorithm(LearningAlgorithm):
-    def __init__(self):
-        self.bid = 0.5  # Initial bid
-        self.learning_rate = 0.01  # Learning rate
-
-    def update(self, auction_result):
-        # Update the primal-dual algorithm based on the auction result
-        if auction_result:
-            # If the auction was won, increase the bid
-            self.bid += self.learning_rate
+class UCB1Agent:
+    def __init__(self, K, T, range=1):
+        self.K = K
+        self.T = T
+        self.range = range
+        self.a_t = None
+        self.average_rewards = np.zeros(K)
+        self.N_pulls = np.zeros(K)
+        self.t = 0
+    
+    def pull_arm(self):
+        if self.t < self.K:
+            self.a_t = self.t 
         else:
-            # If the auction was lost, decrease the bid
-            self.bid -= self.learning_rate
-        # Ensure the bid stays within the valid range
-        self.bid = np.clip(self.bid, 0, 1)
-
-# Main part of the code
-pricing_strategy = PricingStrategy()
-auction = Auction()
-learning_algorithm = PrimalDualAlgorithm()
-
-prices = pricing_strategy.discretize_prices()
-bids = np.random.rand(10)  # 10 agents
-auction_result = auction.run(bids)
-learning_algorithm.update(auction_result)
+            ucbs = self.average_rewards + self.range*np.sqrt(2*np.log(self.T)/self.N_pulls)
+            self.a_t = np.argmax(ucbs)
+        return self.a_t
+    
+    def update(self, r_t):
+        self.N_pulls[self.a_t] += 1
+        self.average_rewards[self.a_t] += (r_t - self.average_rewards[self.a_t])/self.N_pulls[self.a_t]
+        self.t += 1

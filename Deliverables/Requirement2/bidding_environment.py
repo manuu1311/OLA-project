4181dfae6267a168764fe9a171a16fcae90a817d
nicolo tocalli <nicolo.tocalli@mail.polimi.it>
2024-05-07 15:26:11 +0200

    Bidding and pricing environment skeletons added + first sketch of non_stationary conversion probability function to be tuned + [testing notebook]

---
+++ Deliverables/Requirement2/bidding_environment.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+
+class Auction:
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def get_winners(self, bids):
+        pass
+
+    def get_payments_per_click(self, winners, values, bids):
+        pass
+
+    def round(self, bids):
+        winners, values = self.get_winners(bids) # allocation mechanism!
+        payments_per_click = self.get_payments_per_click(winners, values, bids)
+        return winners, payments_per_click
+
+
+class FirstPriceAuction(Auction):
+    def __init__(self, ctrs):
+        self.ctrs = ctrs
+        self.n_adv = len(self.ctrs)
+    
+    def get_winners(self, bids):
+        adv_values = self.ctrs*bids
+        adv_ranking = np.argsort(adv_values)
+        winner = adv_ranking[-1]
+        return winner, adv_values
+    
+    def get_payments_per_click(self, winners, values, bids):
+        payment = bids[winners]
+        return payment.round(2)

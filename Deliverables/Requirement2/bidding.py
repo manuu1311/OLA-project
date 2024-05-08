9ec2842a9f2b6002ab6e60e61012597d57e9053d
Fabrizio Sordetti <fabrizio.sordetti@gmail.com>
2024-05-08 15:01:32 +0200

    added bidding strategy: Generalized Multiplicative Pacing for Non-Truthful Auctions

--- Deliverables/Requirement2/bidding.py
+++ Deliverables/Requirement2/bidding.py
@@ -1,32 +1,50 @@
 import numpy as np
 
-class Auction:
-    def __init__(self, *args, **kwargs):
-        pass
+class HedgeAgent:
+    def __init__(self, K, learning_rate):
+        self.K = K
+        self.learning_rate = learning_rate
+        self.weights = np.ones(K)
+        self.x_t = np.ones(K)/K
+        self.a_t = None
+        self.t = 0
 
-    def get_winners(self, bids):
-        pass
-
-    def get_payments_per_click(self, winners, values, bids):
-        pass
-
-    def round(self, bids):
-        winners, values = self.get_winners(bids) # allocation mechanism!
-        payments_per_click = self.get_payments_per_click(winners, values, bids)
-        return winners, payments_per_click
-
-class FirstPriceAuction(Auction):
-    def __init__(self, ctrs):
-        self.ctrs = ctrs
-        self.n_adv = len(self.ctrs)
+    def pull_arm(self):
+        self.x_t = self.weights/sum(self.weights)
+        self.a_t = np.random.choice(np.arange(self.K), p=self.x_t)
+        return self.a_t
     
-    def get_winners(self, bids):
-        adv_values = self.ctrs*bids
-        adv_ranking = np.argsort(adv_values)
-        winner = adv_ranking[-1]
-        return winner, adv_values
+    def update(self, l_t):
+        self.weights *= np.exp(-self.learning_rate*l_t)
+        self.t += 1
     
-    def get_payments_per_click(self, winners, values, bids):
-        payment = values[winners]/self.ctrs[winners]
-        return payment.round(2)
+class FFMultiplicativePacingAgent:
+    def __init__(self, bids_set, valuation, budget, T, eta):
+        self.bids_set = bids_set
+        self.K = len(bids_set)
+        self.hedge = HedgeAgent(self.K, np.sqrt(np.log(self.K)/T))
+        self.valuation = valuation
+        self.budget = budget
+        self.eta = eta
+        self.T = T
+        self.rho = self.budget/self.T
+        self.lmbd = 1
+        self.t = 0
+
+    def bid(self):
+        if self.budget < 1:
+            return 0
+        return self.bids_set[self.hedge.pull_arm()]
     
+    def update(self, f_t, c_t, m_t):
+        # update hedge
+        f_t_full = np.array([(self.valuation-b)*int(b >= m_t) for b in self.bids_set])
+        c_t_full = np.array([b*int(b >= m_t) for b in self.bids_set])
+        L = f_t_full - self.lmbd*(c_t_full-self.rho)
+        range_L = 2+(1-self.rho)/self.rho
+        self.hedge.update((2-L)/range_L) # hedge needs losses in [0,1]
+        # update lagrangian multiplier
+        self.lmbd = np.clip(self.lmbd-self.eta*(self.rho-c_t), 
+                            a_min=0, a_max=1/self.rho)
+        # update budget
+        self.budget -= c_t

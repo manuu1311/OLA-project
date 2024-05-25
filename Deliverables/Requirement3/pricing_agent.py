0afaa68415754a011e00db4adaa8873ed9303166
manu <emanuele.pocelli@mail.polimi.it>
2024-05-25 21:16:17 +0200

    fixed 2-dimensional gp: using sklearn implementation. only need to put env and agent together

--- Deliverables/Requirement3/pricing_agent.py
+++ Deliverables/Requirement3/pricing_agent.py
@@ -1,4 +1,6 @@
 import numpy as np
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF
 
 class SWUCBAgent:
     def __init__(self, K, T, W, prices, range=1):
@@ -93,51 +95,23 @@ class CUSUMUCBAgent:
         return False
 
 
-class RBFGaussianProcess:
-    def __init__(self, scale=1, reg=1e-2):
-        self.scale = scale 
-        self.reg = reg
-        self.k_xx_inv = None
-
-    def rbf_kernel_incr_inv(self, B, C, D):
-        temp = np.linalg.inv(D - C @ self.k_xx_inv @ B)
-        block1 = self.k_xx_inv + self.k_xx_inv @ B @ temp @ C @ self.k_xx_inv
-        block2 = - self.k_xx_inv @ B @ temp
-        block3 = - temp @ C @ self.k_xx_inv
-        block4 = temp
-        res1 = np.concatenate((block1, block2), axis=1)
-        res2 = np.concatenate((block3, block4), axis=1)
-        res = np.concatenate((res1, res2), axis=0)
-        return res
-
-    def rbf_kernel(self, a, b):
-        a_ = a.reshape(-1, 1)
-        b_ = b.reshape(-1, 1)
-        output = -1 * np.ones((a_.shape[0], b_.shape[0]))
-        for i in range(a_.shape[0]):
-            output[i, :] = np.power(a_[i] - b_, 2).ravel()
-        return np.exp(-self.scale * output)
+class GPUCB:
+    def __init__(self, T, prices1,prices2, scale=0.1):
+        kernel = RBF(length_scale=scale)
+        self.gp = GaussianProcessRegressor(kernel=kernel)
+        x,y=np.meshgrid(prices1,prices2)
+        self.arms = np.column_stack((x.reshape(-1), y.reshape(-1)))
+        self.gamma = lambda t: np.log(t+1)**2 
+        self.beta = lambda t: 1 + 0.5*np.sqrt(2 * (self.gamma(t) + 1 + np.log(T)))
+        self.t=0
+        self.T=T
+            
+    def pull_arm(self):
+        self.mu_t, self.sigma_t = self.gp.predict(self.arms,return_std=True) 
+        ucbs = self.mu_t + self.beta(self.t) * self.sigma_t
+        self.a_t = np.argmax(ucbs)
+        return self.arms[self.a_t]
     
-    def fit(self, x=np.array([]), y=np.array([])):
-        x,y = np.array(x),np.array(y)
-        if self.k_xx_inv is None:
-            self.y = y.reshape(-1,1)
-            self.x = x.reshape(-1,1)
-            k_xx = self.rbf_kernel(self.x, self.x) + self.reg * np.eye(self.x.shape[0])
-            self.k_xx_inv = np.linalg.inv(k_xx)
-        else:
-            B = self.rbf_kernel(self.x, x)
-            self.x = np.vstack((self.x, x))
-            self.y = np.vstack((self.y, y))
-            self.k_xx_inv = self.rbf_kernel_incr_inv(B, B.T, np.array([1 + self.reg]))
-
-        return self
-
-    def predict(self, x_predict):
-        k = self.rbf_kernel(x_predict, self.x)
-
-        mu_hat = k @ self.k_xx_inv @ self.y
-        sigma_hat = 1 - np.diag(k @ self.k_xx_inv @ k.T)
-
-        return mu_hat.ravel(), sigma_hat.ravel()
-    
+    def update(self, r_t):
+        self.gp = self.gp.fit(self.arms[self.a_t][:,None].T, np.array([r_t]).reshape(-1,1))
+        self.t += 1
